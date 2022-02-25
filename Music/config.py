89cc9f080fb769a04b56060b601799388b739caa
justteen <48372964+justteen@users.noreply.github.com>
2022-02-25 11:38:52 +0700

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 get_queue = {}
-SESSION_NAME = getenv('SESSION_NAME', 'session')
+SESSION_NAME = getenv('SESSION_NAME', 'BQCsvGpRC8w6Y3K1BMbag71Ipo9XiR2t3ZfgjIXN7gmaIAC-PxC_hr79dE7pmCIb6t59WDVPYIrAeqz38M3Vepmk03tRUHz_-AQKUax_O-h4kjnQ278uE-m2lUG6pqQTzuuKc7_qfWNbM6HXlKYuflbRgtW5KPsDjG_dt3a5Y3L1i15QDVuBswhDCEXeepx4Y_2CzqrQV_PS0AWjOJ4CE-F1XHZFsOg3FjJOCYNZduRx0QwWHEzWuVzhTr_4n0uT_tx2BDH8yCEf7YuJxQ34wpC3bKnhELXF5ctRm-B6fLxY51y7Bh9jTwCNEASu3WPbgzubO1Qx_rNIQXPMbcSM7KAAAAAS4Lh4EA')
 BOT_TOKEN = getenv('BOT_TOKEN')
 API_ID = int(getenv('API_ID', "11446036"))
 API_HASH = getenv('API_HASH', "9fb20b211047e876fa3812ed18af2d4b")))
